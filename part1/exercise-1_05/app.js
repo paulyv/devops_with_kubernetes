e2e89301e60e0f47de677997fd6233367937600a
Pauli Varelius <pauli.varelius@gmail.com>
2020-11-11 22:18:03 +0200

    added a new exercise

---
+++ part1/exercise-1_05/app.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const app = express()
+const path = require('path');
+const port = 3000
+
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname + '/index.html'));
+});
+
+app.listen(port, () => {
+  console.log(`Server started at http://localhost:${port}`)
+})
